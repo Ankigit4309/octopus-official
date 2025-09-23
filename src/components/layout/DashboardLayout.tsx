73ad57c4dbc9d6e223987e3e63a784e66586155e
bhautik_dev <bhautik00992002@gmail.com>
2025-09-23 14:30:55 +0530

    UI changes

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,21 +1,3 @@
-// import { Outlet } from 'react-router-dom'
-// import { SidebarProvider } from '@/components/ui/sidebar'
-// import { AppSidebar } from '@/components/layout/AppSidebar'
-
-// const DashboardLayout = () => {
-//   return (
-//     <SidebarProvider>
-//       <div className="min-h-screen flex w-full">
-//         <AppSidebar />
-//         <main className="flex-1 overflow-auto">
-//           <Outlet />
-//         </main>
-//       </div>
-//     </SidebarProvider>
-//   )
-// }
-
-// export default DashboardLayout  
 
 import { Outlet } from 'react-router-dom'
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
